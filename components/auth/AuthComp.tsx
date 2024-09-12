31924fa1db0fe069a388b4d4c0376680ac32a87e
ejim favour <favourejim56@gmail.com>
2024-09-12 06:41:11 +0100

    added responsiveness to the select acc screen

--- components/auth/AuthComp.tsx
+++ components/auth/AuthComp.tsx
@@ -6,49 +6,53 @@ import logo from "../../assets/logo.svg";
 import Link from "next/link";
 
 const AuthComp: React.FC<{
-    children: React.ReactNode;
-    link: string;
-    linkTitle: string;
+  children: React.ReactNode;
+  link: string;
+  linkTitle: string;
 }> = ({ children, link, linkTitle }) => {
-    return (
-        <section className="flex h-screen font-outfit">
-            <div className="w-[50%] flex flex-col justify-between p-[3.8rem] h-screen overflow-y-auto">
-                <div className="flex items-center justify-between">
-                    <Link href={"/"}>
-                        <Image
-                            src={logo}
-                            alt="logo image"
-                            priority
-                            width={200}
-                            height={200}
-                            className="w-auto h-auto"
-                        />
-                    </Link>
+  return (
+    <section className="flex h-screen font-outfit">
+      <div className="w-[50%] xlg:w-[60%] xmd:w-full flex flex-col justify-between sm:justify-start p-[3.8rem] sm:p-[3rem] ssm:p-[2rem] h-screen overflow-y-auto">
+        <div className="flex items-center justify-between">
+          <Link href={"/"}>
+            <Image
+              src={logo}
+              alt="logo image"
+              priority
+              width={200}
+              height={200}
+              className="w-auto h-auto"
+            />
+          </Link>
 
-                    <div className="flex items-center">
-                        <p className="text-[#22222299]">
-                            Already have an account? {" "}
-                        </p>
-                        <Link href={link} className="text-[#8342FF]">
-                            {linkTitle}
-                        </Link>
-                    </div>
-                </div>
-                <div>{children}</div>
-                <p className="text-[#222222CC]">© 2024 TicketHive</p>
-            </div>
-            <div className="w-[50%] ">
-                <Image
-                    src={authImg}
-                    alt={"auth pages image"}
-                    priority
-                    width={600}
-                    height={600}
-                    className="w-full h-full"
-                />
-            </div>
-        </section>
-    );
+          <div className="flex items-center sm:hidden">
+            <p className="text-[#22222299]">Already have an account?  </p>
+            <Link href={link} className="text-[#8342FF]">
+              {linkTitle}
+            </Link>
+          </div>
+        </div>
+        <div>{children}</div>
+        <div className="items-center hidden sm:flex justify-center mb-[2rem]">
+          <p className="text-[#22222299]">Already have an account?  </p>
+          <Link href={link} className="text-[#8342FF]">
+            {linkTitle}
+          </Link>
+        </div>
+        <p className="text-[#222222CC] sm:mt-auto">© 2024 TicketHive</p>
+      </div>
+      <div className="w-[50%] xlg:w-[40%] xmd:hidden ">
+        <Image
+          src={authImg}
+          alt={"auth pages image"}
+          priority
+          width={600}
+          height={600}
+          className="w-full h-full"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default AuthComp;
--- components/home/HomeComp.tsx
+++ components/home/HomeComp.tsx
@@ -11,69 +11,69 @@ import BringYourVision from "./BringYourVision";
 import WhyTicketHive from "../WhyTicketHive";
 
 const HomeComp = () => {
-    return (
-        <main className="font-outfit">
-            <SectionCover className="h-[calc(100vh-9rem)] bg-home-1 bg-no-repeat bg-center py-[5rem]     flex">
-                <div className="w-[50%] flex flex-col justify-end">
-                    <h2 className="text-[7rem] text-[rgba(255,255,255,0.88)] flex flex-col leading-[7rem]">
-                        <span> Acquire tickets</span>
-                        <span>with ease</span>
-                    </h2>
-                    <p className="text-[rgba(255,255,255,0.75)] text-[2rem] mt-[2.4rem] mb-[4rem]">
-                        Convenient and stress-free solutions for both attendees
-                        and organizers, designed to make your event planning
-                        seamless and enjoyable.
-                    </p>
-                    <div>
-                        <button
-                            type="button"
-                            className="relative bg-color-purple-1 text-color-white-1 blurred-button px-[3.5rem] py-[1.5rem] rounded-[0.6rem] mr-[2rem] hover:bg-[rgba(243,237,255,1)] hover:text-color-purple-1 transition-all duration-150 ease-in "
-                        >
-                            <span className="absolute blur-item bottom-0 top-0 left-0 right-0 bg-[rgba(255,255,255,0.2)] z-20 rounded-[0.6rem]"></span>
-                            <span className=" z-30 relative">Get ticket</span>
-                        </button>
-                        <button
-                            type="button"
-                            className="bg-[rgba(243,237,255,1)] text-color-purple-1 px-[3.5rem] py-[1.5rem] rounded-[0.6rem] hover:bg-color-purple-1 hover:text-color-white-1 transition-all duration-150 ease-in"
-                        >
-                            Create Event
-                        </button>
-                    </div>
-                </div>
-                <div className="p-[1rem] rounded-[1.6rem] self-start ml-auto flex flex-col relative overflow-hidden">
-                    <div className="absolute blur-item bottom-0 z-20 top-0 left-0 right-0 bg-[rgba(255,255,255,0.2)] rounded-[1.6rem]"></div>
-                    <Image
-                        src={sideHomeImg}
-                        alt="Image of the event"
-                        priority
-                        width={300}
-                        height={200}
-                        className="w-[30rem] h-[20rem] blur-none rounded-[1.6rem] z-30 relative"
-                    />
-                    <p className="text-color-white-1 my-[0.8rem] uppercase z-30 relative">
-                        Ticket Hive
-                    </p>
-                    <div className="flex items-center text-color-white-1 mb-[0.5rem] z-30 relative">
-                        <IoLocationOutline className="text-color-current mr-[0.5rem]" />
-                        <p>Balmoral Convention Center Ikeja</p>
-                    </div>
-                    <div className="flex items-center text-color-white-1 mb-[0.5rem] z-30 relative">
-                        <MdAccessTime className="text-color-current mr-[0.5rem] " />
-                        <p>Wed, Aug 7 • 8:30 AM</p>
-                    </div>
-                </div>
-            </SectionCover>
-            <EventCategories />
-            <section className="pb-[10rem]">
-                <EventSlider title={"trending events"} />
-            </section>
-            <BringYourVision />
-            <section className="py-[10rem]">
-                <EventSlider title={"More events"} />
-            </section>
-            <WhyTicketHive />
-        </main>
-    );
+  return (
+    <main className="font-outfit">
+      <SectionCover className="h-[calc(100vh-9rem)] bg-home-1 bg-no-repeat bg-center py-[5rem] bg-cover     flex">
+        <div className="w-[50%] flex flex-col justify-end">
+          <h2 className="text-[7rem] text-[rgba(255,255,255,0.88)] flex flex-col leading-[7rem]">
+            <span> Acquire tickets</span>
+            <span>with ease</span>
+          </h2>
+          <p className="text-[rgba(255,255,255,0.75)] text-[2rem] mt-[2.4rem] mb-[4rem]">
+            Convenient and stress-free solutions for both attendees and
+            organizers, designed to make your event planning seamless and
+            enjoyable.
+          </p>
+          <div>
+            <button
+              type="button"
+              className="relative bg-color-purple-1 text-color-white-1 blurred-button px-[3.5rem] py-[1.5rem] rounded-[0.6rem] mr-[2rem] hover:bg-[rgba(243,237,255,1)] hover:text-color-purple-1 transition-all duration-150 ease-in "
+            >
+              <span className="absolute blur-item bottom-0 top-0 left-0 right-0 bg-[rgba(255,255,255,0.2)] z-20 rounded-[0.6rem]"></span>
+              <span className=" z-30 relative">Get ticket</span>
+            </button>
+            <button
+              type="button"
+              className="bg-[rgba(243,237,255,1)] text-color-purple-1 px-[3.5rem] py-[1.5rem] rounded-[0.6rem] hover:bg-color-purple-1 hover:text-color-white-1 transition-all duration-150 ease-in"
+            >
+              Create Event
+            </button>
+          </div>
+        </div>
+        <div className="p-[1rem] rounded-[1.6rem] self-start ml-auto flex flex-col relative overflow-hidden">
+          <div className="absolute blur-item bottom-0 z-20 top-0 left-0 right-0 bg-[rgba(255,255,255,0.2)] rounded-[1.6rem]"></div>
+          <Image
+            src={sideHomeImg}
+            alt="Image of the event"
+            priority
+            width={300}
+            height={200}
+            className="w-[30rem] h-[20rem] blur-none rounded-[1.6rem] z-30 relative"
+          />
+          <p className="text-color-white-1 my-[0.8rem] uppercase z-30 relative">
+            Ticket Hive
+          </p>
+          <div className="flex items-center text-color-white-1 mb-[0.5rem] z-30 relative">
+            <IoLocationOutline className="text-color-current mr-[0.5rem]" />
+            <p>Balmoral Convention Center Ikeja</p>
+          </div>
+          <div className="flex items-center text-color-white-1 mb-[0.5rem] z-30 relative">
+            <MdAccessTime className="text-color-current mr-[0.5rem] " />
+            <p>Wed, Aug 7 • 8:30 AM</p>
+          </div>
+        </div>
+      </SectionCover>
+      <EventCategories />
+      <section className="pb-[10rem]">
+        <EventSlider title={"trending events"} />
+      </section>
+      <BringYourVision />
+      <section className="py-[10rem]">
+        <EventSlider title={"More events"} />
+      </section>
+      <WhyTicketHive />
+    </main>
+  );
 };
 
 export default HomeComp;
