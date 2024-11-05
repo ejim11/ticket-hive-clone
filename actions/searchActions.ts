13dcaa5251c32514d0911e07e9697d69c45ef67f
ejim favour <favourejim56@gmail.com>
2024-11-05 12:41:55 +0100

    fixing filtering issues

--- actions/searchActions.ts
+++ actions/searchActions.ts
@@ -3,34 +3,37 @@ import { searchForEvent } from "@/services/searchService";
 import { eventsActions } from "@/slices/eventSlice";
 
 export const searchForEventDispatch =
-    (
-        eventName?: string,
-        category?: string,
-        date?: string,
-        price?: string,
-        attendance?: string,
-        sort?: string,
-        sortFunc?: Function
-    ) =>
-    async (dispatch: Function) => {
-        dispatch(eventsActions.toggleIsLoading(true));
-        try {
-            const res = await searchForEvent(
-                eventName,
-                category,
-                date,
-                price,
-                attendance
-            );
+  (
+    eventName?: string,
+    category?: string,
+    date?: string,
+    price?: string,
+    attendance?: string,
+    sort?: string,
+    sortFunc?: Function
+  ) =>
+  async (dispatch: Function) => {
+    dispatch(eventsActions.toggleIsLoading(true));
+    try {
+      const res = await searchForEvent(
+        eventName,
+        category,
+        date,
+        price,
+        attendance
+      );
 
-            const ev = res.data.data.data;
+      console.log(res);
+      const ev = res.data.data.data;
 
-            const events = sort ? sortEvents(ev, sort) : res.data.data.data;
+      console.log(ev);
 
-            dispatch(eventsActions.setEvents(events));
-            dispatch(eventsActions.toggleIsLoading(false));
-        } catch (error) {
-            console.log(error);
-            dispatch(eventsActions.toggleIsLoading(false));
-        }
-    };
+      const events = sort ? sortEvents(ev, sort) : res.data.data.data;
+
+      dispatch(eventsActions.setEvents(events));
+      dispatch(eventsActions.toggleIsLoading(false));
+    } catch (error) {
+      console.log(error);
+      dispatch(eventsActions.toggleIsLoading(false));
+    }
+  };
