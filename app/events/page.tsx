3975406d65b8afbeab60830a75414f842186907a
ejim favour <favourejim56@gmail.com>
2024-11-05 19:40:15 +0100

    modified and fixed the events page error

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -3,6 +3,7 @@
 import React, { Suspense } from "react";
 import Events from "@/components/events/Events";
 import { useAppSelector } from "@/hooks/customHook";
+import WhyTicketHive from "@/components/WhyTicketHive";
 
 export default function Page() {
   const { events } = useAppSelector((state) => state.event);
@@ -10,7 +11,16 @@ export default function Page() {
   return (
     <>
       <Suspense fallback={<div>Loading...</div>}>
-        <Events />
+        {events.length > 0 ? (
+          <Events />
+        ) : (
+          <div className="">
+            <div className="w-full h-[40rem] flex items-center justify-center">
+              <p>No Event Found.</p>
+            </div>
+            <WhyTicketHive />
+          </div>
+        )}
       </Suspense>
     </>
   );
