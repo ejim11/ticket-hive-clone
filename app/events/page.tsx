84be0f4813509c6c101888809b8f3abe2b11e8a3
ejim favour <favourejim56@gmail.com>
2024-11-05 12:56:19 +0100

    fixing filtering issues

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -4,18 +4,20 @@ import React from "react";
 import Events from "@/components/events/Events";
 
 const page = ({
-    searchParams,
+  searchParams,
 }: {
-    searchParams: {
-        name?: string;
-        category?: string;
-        date?: string;
-        price?: string;
-        attendance?: string;
-        sort?: string;
-    };
+  searchParams: {
+    name?: string;
+    category?: string;
+    date?: string;
+    price?: string;
+    attendance?: string;
+    sort?: string;
+  };
 }) => {
-    return <Events searchParams={searchParams} />;
+  console.log("starting");
+  console.log(searchParams);
+  return <Events searchParams={searchParams} />;
 };
 
 export default page;
