9dd52358d65048f93a6082dc53de19a0ca2d6e17
ejim favour <favourejim56@gmail.com>
2024-11-05 02:31:59 +0100

    made the search query to be lower case when sending

--- services/searchService.ts
+++ services/searchService.ts
@@ -1,14 +1,14 @@
 import { events } from "@/axios.config";
 
 export const searchForEvent = async (
-    eventName?: string,
-    category?: string,
-    date?: string,
-    price?: string,
-    attendance?: string
+  eventName?: string,
+  category?: string,
+  date?: string,
+  price?: string,
+  attendance?: string
 ) => {
-    console.log(category);
-    return await events.get(
-        `?name=${eventName}&category=${category}&date=${date}&price=${price}&attendance=${attendance}`
-    );
+  console.log(category);
+  return await events.get(
+    `?name=${eventName?.toLowerCase()}&category=${category}&date=${date}&price=${price}&attendance=${attendance}`
+  );
 };
