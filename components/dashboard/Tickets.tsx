dc9d1e3559818e3f179b0bed6e8fc2e7e5fb91be
ejim11 <favourejim56@gmail.com>
2024-11-18 20:02:54 +0100

    finish v1.0.0

--- components/dashboard/Tickets.tsx
+++ components/dashboard/Tickets.tsx
@@ -6,27 +6,27 @@ import TicketInventory from "./main/TicketInventory";
 import { eventsDashboardData } from "../utils/data";
 
 const Tickets = () => {
-    const addTicketsHandler = (e: any) => {};
+  const addTicketsHandler = (e: any) => {};
 
-    const [monthFilter, setMonthFilter] = useState<any>("");
+  const [monthFilter, setMonthFilter] = useState<any>("");
 
-    return (
-        <DashboardCover
-            title="Tickets"
-            btnText="Add tickets"
-            onClickBtn={addTicketsHandler}
-        >
-            <SearchAndFilterSection
-                monthFilter={monthFilter}
-                setMonthFilter={setMonthFilter}
-            />
-            <TicketInventory
-                month={monthFilter}
-                data={eventsDashboardData}
-                itemsInPage={100}
-            />
-        </DashboardCover>
-    );
+  return (
+    <DashboardCover
+      title="Tickets"
+      btnText="Add tickets"
+      onClickBtn={addTicketsHandler}
+    >
+      <SearchAndFilterSection
+        monthFilter={monthFilter}
+        setMonthFilter={setMonthFilter}
+      />
+      <TicketInventory
+        month={monthFilter}
+        // data={eventsDashboardData}
+        itemsInPage={100}
+      />
+    </DashboardCover>
+  );
 };
 
 export default Tickets;
